ab0915409fe0b2fa6ad09775d096fcdb473eaf78
Fu Xiang <orfuxiang2015@gmail.com>
2025-08-20 16:52:12 +0800

    Added echoing, and ending on 'bye'

--- src/main/java/Heimerdinger.java
+++ src/main/java/Heimerdinger.java
@@ -2,7 +2,18 @@ import java.util.Scanner;
 
 public class Heimerdinger {
     public static void main(String[] args) {
+        boolean end = false;
+
         System.out.println("Hello! I'm Heimerdinger\nWhat can I do for you?\n");
+        Scanner scanner = new Scanner(System.in);
+        while (!end) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input + "\n");
+        }
         System.out.println("For Piltover and science! Goodbye!");
+        scanner.close();
     }
 }
