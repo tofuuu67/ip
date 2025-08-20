d9df226ebef50b320fe0a35faa8d3861c705a0fa
Fu Xiang <orfuxiang2015@gmail.com>
2025-08-20 17:19:39 +0800

    Added add to list and display list functionality

--- src/main/java/Heimerdinger.java
+++ src/main/java/Heimerdinger.java
@@ -6,12 +6,26 @@ public class Heimerdinger {
 
         System.out.println("Hello! I'm Heimerdinger\nWhat can I do for you?\n");
         Scanner scanner = new Scanner(System.in);
+        String[] array = new String[100];
+        int index = 0;
         while (!end) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                if (index == 0) {
+                    System.out.println("Hmm its an empty list!");
+                } else {
+                    for (int i = 0; i < index; i++) {
+                        System.out.println(i + 1 + ". " + array[i]);
+                    }
+                    System.out.println("\n");
+                }
+            } else {
+                System.out.println("Noted: " + input + "\n");
+                array[index] = input;
+                index++;
             }
-            System.out.println(input + "\n");
         }
         System.out.println("For Piltover and science! Goodbye!");
         scanner.close();
