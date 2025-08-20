ede6ec19c7efa365c4e5ab0783bd07e9ababde56
Fu Xiang <orfuxiang2015@gmail.com>
2025-08-20 18:01:37 +0800

    Added task class, allow for marking and unmarking of tasks

--- src/main/java/Heimerdinger.java
+++ src/main/java/Heimerdinger.java
@@ -6,28 +6,62 @@ public class Heimerdinger {
 
         System.out.println("Hello! I'm Heimerdinger\nWhat can I do for you?\n");
         Scanner scanner = new Scanner(System.in);
-        String[] array = new String[100];
+        Task[] array = new Task[100];
+        String input;
         int index = 0;
         while (!end) {
-            String input = scanner.nextLine();
+            input = scanner.nextLine();
+            String split[] = input.split(" ", 2);
+            String firstWord = split[0];
+            String secondWord = (split.length > 1) ? split[1] : "";
+
             if (input.equals("bye")) {
                 break;
             } else if (input.equals("list")) {
-                if (index == 0) {
-                    System.out.println("Hmm its an empty list!");
-                } else {
-                    for (int i = 0; i < index; i++) {
-                        System.out.println(i + 1 + ". " + array[i]);
-                    }
-                    System.out.println("\n");
+                if (index > 0) {
+                    System.out.println("One step closer to greater understanding! Here's your list:");
                 }
+                showList(index, array);
+            } else if (firstWord.equals("mark") && isNumeric(secondWord)){
+                System.out.println("Indeed, a wise choice! Here's your list:");
+                int listNumber = Integer.parseInt(secondWord);
+                array[listNumber - 1].markAsDone();
+                showList(index, array);
+            } else if (firstWord.equals("unmark") && isNumeric(secondWord)){
+                System.out.println("One step forward, two steps back... Anyways here's your list:");
+                int listNumber = Integer.parseInt(secondWord);
+                array[listNumber - 1].markAsNotDone();
+                showList(index, array);
             } else {
                 System.out.println("Noted: " + input + "\n");
-                array[index] = input;
+                array[index] = new Task(input);
                 index++;
             }
         }
         System.out.println("For Piltover and science! Goodbye!");
         scanner.close();
+    }
+
+    public static void showList(int listLength, Task[] array) {
+        if (listLength == 0) {
+            System.out.println("Hmm its an empty list!");
+        } else {
+            for (int i = 0; i < listLength; i++) {
+                System.out.println(i + 1 + ".[" + array[i].getStatusIcon() + "] " + array[i].toString());
+            }
+            System.out.println("\n");
+        }
+    }
+
+    public static boolean isNumeric(String maybeNumber) {
+        if (maybeNumber == null) {
+            return false;
+        }
+        try {
+            Double.parseDouble(maybeNumber);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
     }
 }
