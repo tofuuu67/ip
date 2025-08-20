c1bbdcce9f7ac2567f347cbe5ee4186dc352a580
Fu Xiang <orfuxiang2015@gmail.com>
2025-08-20 16:33:56 +0800

    Renamed chatbot, add greet and exit lines

---
+++ src/main/java/Heimerdinger.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Heimerdinger {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Heimerdinger\nWhat can I do for you?\n");
+        System.out.println("For Piltover and science! Goodbye!");
+    }
+}
